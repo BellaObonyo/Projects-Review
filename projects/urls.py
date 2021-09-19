c7f818b6d6e9d7aee0ac0a7ecd54eacc9d3e9367
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-19 17:04:28 +0300

    updared urls.py file

---
+++ projects/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('portfolio/', views.portfolio, name='portfolio'),
+  path('about/', views.about, name='about'),
+  path('services/', views.services, name='services'),
+  path('contact/', views.contact, name='contact'),
+  path('resume/', views.resume, name='resume'),
+  path('search/',views.search_portfolio,name='search_portfolio'),
+  path('portfolio/details/<int:portfolio_id>',views.detail,  name='portfolio_details')
+]
+
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
